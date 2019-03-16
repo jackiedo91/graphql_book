b427c5bc5b32a3a8af50b94d8589bf3c31103b1b
Huy Do <huydo91@outlook.com>
2019-03-16 12:04:04 +0700

    add orm sequelize to handle mysql database

---
+++ src/server/config/index.js
@@ -0,0 +1,33 @@
+// Config for database
+
+module.exports = {
+  "development": {
+    "username": "devuser",
+    "password": "123456789",
+    "database": "graphbook_dev",
+    "host": "localhost",
+    "dialect": "mysql",
+    "operatorsAliases": false,
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    }
+  },
+  "production": {
+    "host": process.env.host,
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": process.env.database,
+    "logging": false,
+    "dialect": "mysql",
+    "operatorsAliases": false,
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    }
+  }
+}
---
+++ src/server/models/post.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    text: DataTypes.TEXT
+  }, {});
+
+  Post.associate = function(models) {
+    // associations can be defined here
+  };
+
+  return Post;
+};
