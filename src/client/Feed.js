e592d746f0a71910bbbb0b981f83fed6349793ba
Huy Do <huydo91@outlook.com>
2019-03-17 09:21:38 +0700

    using apollo with query component

--- src/client/Feed.js
+++ src/client/Feed.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../../assets/css/style.css';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 const GET_POSTS = gql`{
   posts {
@@ -51,48 +51,43 @@ class Feed extends Component {
   }
 
   render() {
-    const { posts, loading, error } = this.props;
     const { postContent } = this.state;
-
-    if(loading) {
-      return "Loading...";
-    }
-    if(error) {
-      return error.message;
-    }
-
 
     return (
       <div className="container">
-        <div className="postForm">
-          <form onSubmit={this.handleSubmit}>
-            <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!"/>
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-
-        <div className="feed">
-          {posts.map((post, i) =>
-            <div key={post.id} className="post">
-              <div className="header">
-                <img src={post.user.avatar} />
-                <h2>{post.user.username}</h2>
-              </div>
-              <p className="content">
-                {post.text}
-              </p>
-            </div>
-          )}
-        </div>
+      <div className="postForm">
+        <form onSubmit={this.handleSubmit}>
+          <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!"/>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
+
+      <div className="feed">
+        <Query query={GET_POSTS}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return error.message;
+
+            const { posts } = data;
+
+            return posts.map((post, i) =>
+              <div key={post.id} className="post">
+                <div className="header">
+                  <img src={post.user.avatar} />
+                  <h2>{post.user.username}</h2>
+                </div>
+                <p className="content">
+                  {post.text}
+                </p>
+              </div>
+            )
+          }}
+        </Query>
+      </div>
+    </div>
     )
   }
 }
 
-export default graphql(GET_POSTS, {
-  props: ({ data: { loading, error, posts }} ) => ({
-    loading,
-    posts,
-    error
-  })
-})(Feed)
+
+export default Feed;
